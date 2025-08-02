a43ea2a1f118ceda147cce287891fa56a7ec782b
Luis Fernando <devfpluis@gmail.com>
2025-08-02 01:13:39 -0300

    render function and django templates

--- recipes/apps.py
+++ recipes/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ReciplesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reciples'
+    name = 'recipes'
--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'recipes/home.html', context={
+        'name' : 'Luis Fernando'
+    })
 
 def sobre(request):
     return HttpResponse('Sobre')
