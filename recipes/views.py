9e5f3bcb0d55929eb7b5afb809fdef00d707432b
Luis Fernando <devfpluis@gmail.com>
2025-08-02 05:16:09 -0300

    search bar

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={
-        'name' : 'Luis Fernando'
-    })
+    return render(request, 'recipes/pages/home.html')
